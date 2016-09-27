ac41bf5f5b5c38943ee43f3ae0b89f50f64b5940
Alex7974115 <blackmesa@hardcore.ru>
2016-09-27 18:10:00 +0300

    Коммит №2. Исправления.

--- src/test/java/LehaTest2.java
+++ src/test/java/LehaTest2.java
@@ -1,5 +1,8 @@
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
 
@@ -9,21 +12,21 @@ import org.openqa.selenium.By;
 public class LehaTest2 {
 
     WebDriver driver = new ChromeDriver();
+    WebDriverWait wait = new WebDriverWait(driver, 10);
+
+
 
     @Test
 
     public void test1() throws Exception {
 
         driver.get("https://vk.com/");
-        driver.findElement(By.xpath("//input[@id='index_email']")).sendKeys("");
-        driver.findElement(By.xpath("//input[@id='index_pass']")).sendKeys("");
+        driver.findElement(By.id("index_email")).sendKeys("electroalex2009@yandex.ru");
+        driver.findElement(By.id("index_pass")).sendKeys("19911991010679");
         driver.findElement(By.xpath("//button[@id='index_login_button']")).click();
-        Thread.sleep(500);
-        driver.findElement(By.xpath("//li[@id='l_msg']")).click();
-        Thread.sleep(1000);
-        driver.findElement(By.xpath("//li[@data-peer='6897955']")).click();
-        Thread.sleep(500);
-        driver.findElement(By.xpath("//div[@id='im_editable0']")).sendKeys("Леха написал автотест");
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//li[@id='l_msg']"))).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[@class='nim-dialog--name-w _im_dialog_name']/span"))).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='im_editable0']"))).sendKeys("Леха написал автотест");
         driver.findElement(By.xpath("//button[@class='flat_button fl_r _im_send']")).click();
           }
 
