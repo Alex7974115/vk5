ce82e5d6edab344d4220164ab38fbcf8131e87c9
Alex7974115 <blackmesa@Hardcore.ru>
2016-09-28 15:34:24 +0300

    Update LehaTest2.java

--- src/test/java/LehaTest2.java
+++ src/test/java/LehaTest2.java
@@ -4,8 +4,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
-
-
 import java.io.FileInputStream;
 import java.util.Properties;
 
